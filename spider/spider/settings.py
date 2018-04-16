be6cf78310283d5698a8bb4ba9b944138215f919
Rajiv Anisetti <rajiv.anisetti@gmail.com>
2018-04-16 06:45:07 -0700

    set up splash

--- spider/spider/settings.py
+++ spider/spider/settings.py
@@ -15,6 +15,21 @@ SPIDER_MODULES = ['spider.spiders']
 NEWSPIDER_MODULE = 'spider.spiders'
 SPLASH_URL = 'http://192.168.59.103:8050'
 
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'spider (+http://www.yourdomain.com)'
--- spider/spider/spiders/job_spider.py
+++ spider/spider/spiders/job_spider.py
@@ -1,1 +1,19 @@
 import scrapy
+from scrapy_splash import SplashRequest
+
+
+class JobSpider(scrapy.Spider):
+	name = "jobs"
+
+	def __init__(self, category=None, keyword=None *args, **kwargs):
+		super(JobSpider, self).__init__(*args, **kwargs)
+		self.category = category
+		self.keyword = keyword
+
+	def start_requests(self):
+		yield SplashRequest(url = 'https://www.federalreserve.gov/start-job-search.htm', callback = self.parse)
+
+	def parse(self, response):
+
+
+
